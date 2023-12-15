9294a714204852a3766ce150b3bfa98e85aa1b90
Ben Cyrus <bencyrus@Bens-MacBook-Air.local>
2023-12-14 19:09:35 -0500

    redirect / to booking

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: '/booking',
+        source: '/',
         destination: '/booking/selection',
         permanent: true,
       },
